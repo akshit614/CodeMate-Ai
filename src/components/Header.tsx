50a959349a8deeba6a10a76a02fb5303decdd141
Aksh4code <akkisingh8266@gmail.com>
2024-10-14 23:41:25 +0530

    completed header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='flex justify-between items-center px-20 py-10'>
+      <Link to={'/'} className='font-bold text-3xl'>CodeMate</Link>
+      <ul className='flex items-center gap-x-3 text-base '>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>
+          <Link to={'/code'} className='bg-gray-700 text-white rounded-full px-4 py-2 transition duration-300 ease-in-out hover:bg-black hover:shadow-[0_0_10px_3px_rgba(255,255,0,1)]'>Let's Try</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
