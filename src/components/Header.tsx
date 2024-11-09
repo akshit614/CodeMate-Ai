9f8c81175092981f47dfbf663ddaac18d4b91ada
Aksh4code <akkisingh8266@gmail.com>
2024-11-09 23:43:46 +0530

    minor updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,6 @@ const Header = () => {
       <Link to={'/'} className='font-bold text-3xl'>CodeMate</Link>
       <ul className='flex items-center gap-x-3 text-base '>
         <li><Link to={'/'}>Home</Link></li>
-        <li>About</li>
-        <li>Contact</li>
         <li>
           <Link  to={'/code'} className='bg-gray-900 text-white rounded-full px-4 py-2 transition duration-300 ease-in-out hover:bg-black hover:shadow-[0_0_10px_3px_rgba(255,255,0,1)]'> Try Now ! </Link>
         </li>
