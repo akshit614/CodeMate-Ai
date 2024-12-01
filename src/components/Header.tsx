b30c66b51c08fdbb6120eec24561d8f97dc93758
Aksh4code <akkisingh8266@gmail.com>
2024-12-01 19:17:45 +0530

    made responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav className='flex justify-between items-center px-20 py-10'>
-      <Link to={'/'} className='font-bold text-3xl'>CodeMate</Link>
+    <nav className='flex justify-between items-center px-5 py-10 md:px-20 md:py-10'>
+      <Link to={'/'} className='font-extrabold text-2xl md:text-3xl'>CodeMate</Link>
       <ul className='flex items-center gap-x-3 text-base '>
         <li><Link to={'/'}>Home</Link></li>
-        <li>
-          <Link  to={'/code'} className='bg-gray-900 text-white rounded-full px-4 py-2 transition duration-300 ease-in-out hover:bg-black hover:shadow-[0_0_10px_3px_rgba(255,255,0,1)]'> Try Now ! </Link>
+        <li className='transform transition-transform duration-300 hover:scale-110 hover:shadow-lg'>
+          <Link  to={'/code'} className='bg-gray-900 font-semibold text-white rounded-full px-4 py-2 ease-in-out hover:bg-black hover:shadow-[0_0_10px_3px_rgba(255,255,0,1)]'> Get Started! </Link>
         </li>
       </ul>
     </nav>
--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,71 +1,95 @@
-
-import { useEffect,useState } from 'react'
-import { FaArrowRight } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { AxiosClient } from '../utils/axios'
-import PlaceholderLoading from 'react-placeholder-loading'
+import { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { AxiosClient } from "../utils/axios";
+import PlaceholderLoading from "react-placeholder-loading";
 
 const DashBoard = () => {
-
   type Data = {
-    input: string
-    _id : string
-  }
+    input: string;
+    _id: string;
+  };
 
-  const [data, setData] = useState<Data[]>([])
-  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<Data[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     try {
-      const res = await AxiosClient.get('/history')
-      const data = await res.data
-      setData(data)
-      } catch (error : any) {
-        console.log("Questions fetching error: " + error)
-    }finally {
-      setLoading(false)
+      const res = await AxiosClient.get("/history");
+      const data = await res.data;
+      setData(data);
+    } catch (error: any) {
+      console.log("Questions fetching error: " + error);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
-  useEffect(() =>{
-    fetchData()
-  },[])
-  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div className="prose">
-        <h1 className='text-blue-900'>
-          Welcome to Code Mate 👋
-        </h1 >
-        <h4 className ="px-9">Get your needed code snippets now with CodeMate !</h4>
-      </div>
-      <div>
-        <h1 className='text-lg py-5'>Previous searched queries  </h1>
-        <div className="flex flex-col  gap-y-4 ">
-          {
-          loading ? 
-          <div>
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          <PlaceholderLoading shape="rect" width={1000} height={30}/><br />
-          </div>
-          :
-            data && data.length>0 && data.slice().reverse().map((item,key) => {
-              return <Link to={`/code/${item._id}`} key={key} className=' justify-between px-4 py-3 w-full border rounded-lg flex items-center'>
-                <h1 className='font-bold'>{item.input}</h1>
-                <button className='outline-none flex gap-x-2 items-center text-blue-600'>Read <FaArrowRight/></button>
-                </Link>
-            })
-          }
+      <div className="p-1 flex flex-col justify-center items-center">
+        <h1 className="flex-col text-blue-900 text-3xl md:text-5xl font-extrabold">Welcome to Code Mate 👋</h1>
+        <h4 className=" md:px-6 pt-1 text-xl font-semibold">
+          Get your code snippets & answers here !
+        </h4>
+        <div className=" flex justify-center items-center md:py-10 py-5">
+          <p className="text-slate-900 font-semibold text-xl pr-2 ">Have a Question - </p>
+          <Link to={'/code'}>
+            <button className="px-6 py-2 font-semibold transform transition-transform duration-300 hover:scale-110 hover:shadow-lg text-xl bg-black rounded-full text-white">Ask Now!</button>
+          </Link>
         </div>
       </div>
-    </>  
-  )
-}
+      <div>
+        <h1 className="text-2xl font-bold py-5 ">Previous searched queries </h1>
+        <div className="flex flex-col  gap-y-4 md:px-5 px-1">
+          {loading ? (
+            <div>
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+              <PlaceholderLoading shape="rect" width={1000} height={30} />
+              <br />
+            </div>
+          ) : (
+            data &&
+            data.length > 0 &&
+            data
+              .slice()
+              .reverse()
+              .map((item, key) => {
+                return (
+                  <Link
+                    to={`/code/${item._id}`}
+                    key={key}
+                    className="justify-between transform transition-transform duration-300 hover:scale-105 hover:shadow-md hover:shadow-blue-500 px-4 py-3 w-full border rounded-lg flex items-center"
+                  >
+                    <h1 className="font-bold">{item.input}</h1>
+                    <button className="outline-none flex gap-x-2 items-center text-blue-600">
+                      Read <FaArrowRight />
+                    </button>
+                  </Link>
+                );
+              })
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default DashBoard
+export default DashBoard;
