4c447d21da551d0d33760764579f2db4f4fd3e4c
Divyanshu <akkisingh8266@gmail.com>
2025-07-09 16:14:43 +0530

    Update gemini.config.js

--- backend/DB/gemini.config.js
+++ backend/DB/gemini.config.js
@@ -1,6 +1,6 @@
 const {GoogleGenerativeAI} = require("@google/generative-ai");
 
-const api_Key = "AIzaSyDxEzd-crQo8dXZhvtd1acaYNZeV3NzEJI";
+const api_Key =  process.env.API_KEY;
 const genAi = new GoogleGenerativeAI(api_Key)
 
 const geminiModel = genAi.getGenerativeModel({
